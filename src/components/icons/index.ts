031e1b20dbc6586fc1b337c11cc0f8907d5c93af
globalenterprisescode <theglobalenterprises@gmail.com>
2025-01-06 11:51:04 +0530

    Add files via upload

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,7 +1,5 @@
 export * from './Upload';
 export * from './Plus';
 export * from './Pencil';
-export * from './ArrowUp';
-export * from './ArrowDown';
 // Re-export existing icons
 export * from '../icons';
